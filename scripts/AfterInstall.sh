38ae3bd31f9580f6673e5c551904331d7df3cc86
waevy <ogonnannamani11@gmail.com>
2022-09-28 12:30:22 +0100

    work

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-npm install
 systemctl enable httpd
 systemctl start httpd
 pm2 restart backend
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/node/Backend-repo
 sudo yum update -y
-sudo yum install -y httpd
-service httpd start
-
